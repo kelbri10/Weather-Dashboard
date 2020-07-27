d91b67770b18a462943c1408a7e74c9f263a4721
kelbri10 <kelbri10@gmail.com>
2020-07-26 23:29:16 -0400

    added general framework of html, added notes to scripts.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//when user searches for city, ajax calls for current and future conditions of the city
+//append the city to the left side of the page as a previous search
+
+//user presented with city name, date, icone representation of weather conditions
+//uv inex displays if weather is favorable, m,oderate or sever
+
+//five day forecast displays the date, icon of weather condition
+
+//when user clicks on city in search history the history is redisplayed 
+
+//when open weather dashboard, last searched city forecast is displayed
